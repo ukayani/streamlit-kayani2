bbd2f8f1105bedfe095428e9b6628a1e6b356677
ukayani <kayani.u@gmail.com>
2025-03-03 20:48:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
+from redshift import Redshift
 
-st.title('kayani2')
-st.write('Welcome to kayani2!')
+conn = Redshift(database="analytics_storysolitaire")
+ttl = 60
+
+st.title("kayani")
+st.write("Welcome to kayani!")
+
+df = conn.query("SELECT * FROM data.payments LIMIT 10", ttl=ttl, index_col="event_time")
+st.dataframe(df)
